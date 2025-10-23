b0b26e705e729309fcd57ff5b7106daec40cd318
king damie <110835422+kingdamie@users.noreply.github.com>
2025-10-23 07:39:31 +0100

    Update footer designer name in Base component

--- src/Components/WeatherApp/Base.jsx
+++ src/Components/WeatherApp/Base.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export const Base = () => {
   return (
     <div className="footer">
-      Designed by <a href="https://github.com/kingdamie">OBINNA-EZE TOBECHUKWU </a>
+      Designed by <a href="https://github.com/kingdamie">King Damie</a>
     </div>
   );
 }
